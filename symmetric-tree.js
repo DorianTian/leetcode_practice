75b32bdef1be4a8b123d33ae0ff664b15b5a372a
Dorian <tianqiyin013368@gmail.com>
2024-12-27 23:22:41 +0800

    Update symmetric-tree.js

--- symmetric-tree.js
+++ symmetric-tree.js
@@ -10,19 +10,41 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+// /**
+//  * @param {TreeNode} root
+//  * @return {boolean}
+//  */
+// const isSymmetric = function(root) {
+//   if (root === null) return false;
+//   const isCurrentNodeSymmetric = function(left, right) {
+//     if (left === null && right === null) return true;
+//     if (!left || !right) return false;
+//     if (left.val !== right.val) return false;
+
+//     return isCurrentNodeSymmetric(left.left, right.right) && isCurrentNodeSymmetric(left.right, right.left);
+//   };
+
+//   return isCurrentNodeSymmetric(root.left, root.right);
+// };
 /**
  * @param {TreeNode} root
  * @return {boolean}
  */
-const isSymmetric = function(root) {
+const isSymmetric = function (root) {
   if (root === null) return false;
-  const isCurrentNodeSymmetric = function(left, right) {
-    if (left === null && right === null) return true;
+
+  const queue = [root.left, root.right];
+  while (queue.length) {
+    let left = queue.shift();
+    let right = queue.shift();
+
+    if (left === null && right === null) continue;
     if (!left || !right) return false;
     if (left.val !== right.val) return false;
 
-    return isCurrentNodeSymmetric(left.left, right.right) && isCurrentNodeSymmetric(left.right, right.left);
-  };
+    queue.push(left.left, right.right);
+    queue.push(left.right, right.left);
+  }
 
-  return isCurrentNodeSymmetric(root.left, root.right);
+  return true;
 };
