add84ad806a152ae29d0a31d2d5c509f6b873da5
Dorian <tianqiyin013368@gmail.com>
2024-12-28 20:11:36 +0800

    Create sum-root-to-leaf-numbers.js

---
+++ sum-root-to-leaf-numbers.js
@@ -0,0 +1,41 @@
+// Input: root = [1,2,3]
+// Output: 25
+// Explanation:
+// The root-to-leaf path 1->2 represents the number 12.
+// The root-to-leaf path 1->3 represents the number 13.
+// Therefore, sum = 12 + 13 = 25.
+// Input: root = [4,9,0,5,1]
+// Output: 1026
+// Explanation:
+// The root-to-leaf path 4->9->5 represents the number 495.
+// The root-to-leaf path 4->9->1 represents the number 491.
+// The root-to-leaf path 4->0 represents the number 40.
+// Therefore, sum = 495 + 491 + 40 = 1026.
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function (root) {
+  let sum = 0;
+  const dfs = function (node, sumCount) {
+    if (!node) return 0;
+
+    sumCount = sumCount * 10 + node.val;
+
+    if (!node.left && !node.right) {
+      return sumCount;
+    }
+
+    return dfs(node.left, sumCount) + dfs(node.right, sumCount);
+  };
+
+  return dfs(root, sum);
+};
