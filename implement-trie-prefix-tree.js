5d9ce5c8aedb0eae54fd86e48202f756490dd63c
Dorian <tianqiyin013368@gmail.com>
2025-01-01 20:59:09 +0800

    Create implement-trie-prefix-tree.js

---
+++ implement-trie-prefix-tree.js
@@ -0,0 +1,79 @@
+// Example 1:
+
+// Input
+// ["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+// [[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+// Output
+// [null, null, true, false, true, null, true]
+
+// Explanation
+// Trie trie = new Trie();
+// trie.insert("apple");
+// trie.search("apple");   // return True
+// trie.search("app");     // return False
+// trie.startsWith("app"); // return True
+// trie.insert("app");
+// trie.search("app");     // return True
+
+const Trie = function () {
+  this.root = {};
+};
+
+/**
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function (word) {
+  let node = this.root;
+  for (let char of word) {
+    if (!node[char]) {
+      node[char] = {};
+    }
+
+    node = node[char];
+  }
+
+  return (node.isEnd = true);
+};
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function (word) {
+  let node = this.root;
+  for (let char of word) {
+    if (!node[char]) {
+      return false;
+    }
+
+    node = node[char];
+  }
+
+  return node.isEnd === true;
+};
+
+/**
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function (prefix) {
+  let node = this.root;
+  for (let char of prefix) {
+    if (!node[char]) {
+      return false;
+    }
+
+    node = node[char];
+  }
+
+  return true;
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
