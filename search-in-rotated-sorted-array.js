18e5c9f9d94df2c51d36d0a22595e91ae7de7aa9
Dorian <tianqiyin013368@gmail.com>
2025-01-05 21:57:07 +0800

    Update search-in-rotated-sorted-array.js

--- search-in-rotated-sorted-array.js
+++ search-in-rotated-sorted-array.js
@@ -15,6 +15,30 @@
  * @param {number} target
  * @return {number}
  */
-const search = function(nums, target) {
+const search = function (nums, target) {
+  let left = 0;
+  let right = nums.length -1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
     
+    if (nums[mid] === target) return mid;
+
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (nums[mid] < target && nums[right] >= target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1
+      }
+    }
+  }
+
+  return -1;
+  
 };
