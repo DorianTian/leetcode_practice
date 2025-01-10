12d767d022e033a8144fade1a93fc61d2a503de5
Dorian <tianqiyin013368@gmail.com>
2025-01-10 23:06:06 +0800

    Create word-break.js

---
+++ word-break.js
@@ -0,0 +1,36 @@
+// Example 1:
+
+// Input: s = "leetcode", wordDict = ["leet","code"]
+// Output: true
+// Explanation: Return true because "leetcode" can be segmented as "leet code".
+// Example 2:
+
+// Input: s = "applepenapple", wordDict = ["apple","pen"]
+// Output: true
+// Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+// Note that you are allowed to reuse a dictionary word.
+// Example 3:
+
+// Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+// Output: false
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+const wordBreak = function (s, wordDict) {
+  const dictSet = new Set(wordDict);
+
+  const dp = Array.from({ length: s.length + 1 }, () => false);
+
+  for (let i = 1; i <= s.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (dp[j] && dictSet.has(s.slice(j, i))) {
+        dp[i] = true;
+        break;
+      }
+    }
+  }
+
+  return dp[s.length];
+};
