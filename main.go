ee0095ae46a6b8f276f0cdcf8834c9e826422956
Dorian <tianqiyin013368@gmail.com>
2025-01-16 23:26:04 +0800

    create first missing positive

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ import (
 func main() {
 	fmt.Println("Main")
 	//nums := [][]int{{1, 4}, {5, 6}}
-	nums := []int{1, 2, 3, 4}
+	nums := []int{3, 4, -1, 1}
 	//k := 3
 	//t := "ABC"
 	//s := "ADOBECODEBANC"
 	//p := "abc"
 	//utils.QuickSort(nums, 0, len(nums)-1)
-	answer := practice.ProductExceptSelf(nums)
+	answer := practice.FirstMissingPositive(nums)
 
 	//fmt.Printf("nums: %v\n", nums)
 	fmt.Printf("answer: %v\n", answer)
