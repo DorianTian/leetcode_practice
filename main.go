b6f63cc3c76c3dd0e30ebb5feb79a156f8612772
Dorian <tianqiyin013368@gmail.com>
2025-01-17 13:24:18 +0800

    create set zeroes

--- main.go
+++ main.go
@@ -8,13 +8,18 @@ import (
 func main() {
 	fmt.Println("Main")
 	//nums := [][]int{{1, 4}, {5, 6}}
-	nums := []int{3, 4, -1, 1}
+	//nums := []int{3, 4, -1, 1}
+	matrix := [][]int{
+		{0, 1, 2, 0},
+		{3, 4, 5, 2},
+		{1, 3, 1, 5},
+	}
 	//k := 3
 	//t := "ABC"
 	//s := "ADOBECODEBANC"
 	//p := "abc"
 	//utils.QuickSort(nums, 0, len(nums)-1)
-	answer := practice.FirstMissingPositive(nums)
+	answer := practice.SetZeroes(matrix)
 
 	//fmt.Printf("nums: %v\n", nums)
 	fmt.Printf("answer: %v\n", answer)
