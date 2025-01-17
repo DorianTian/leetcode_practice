d4c4a63dca2c04a2f0efcedf6089f032e1450049
Dorian <tianqiyin013368@gmail.com>
2025-01-17 23:32:54 +0800

    add spiral order

--- func/practice/practice_go.go
+++ func/practice/practice_go.go
@@ -430,3 +430,44 @@ func SetZeroes(matrix [][]int) [][]int {
 
 	return matrix
 }
+
+func SpiralOrder(matrix [][]int) []int {
+	var result []int
+
+	top := 0
+	bottom := len(matrix) - 1
+	left := 0
+	right := len(matrix[0]) - 1
+
+	for top <= bottom && left <= right {
+		for col := left; col <= right; col++ {
+			result = append(result, matrix[top][col])
+		}
+
+		top++
+
+		for row := top; row <= bottom; row++ {
+			result = append(result, matrix[row][right])
+		}
+
+		right--
+
+		if top <= bottom {
+			for col := right; col >= left; col-- {
+				result = append(result, matrix[bottom][col])
+			}
+
+			bottom--
+		}
+
+		if left <= right {
+			for row := bottom; row >= top; row-- {
+				result = append(result, matrix[row][left])
+			}
+
+			left++
+		}
+	}
+
+	return result
+}
--- main.go
+++ main.go
@@ -10,16 +10,16 @@ func main() {
 	//nums := [][]int{{1, 4}, {5, 6}}
 	//nums := []int{3, 4, -1, 1}
 	matrix := [][]int{
-		{0, 1, 2, 0},
-		{3, 4, 5, 2},
-		{1, 3, 1, 5},
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
 	}
 	//k := 3
 	//t := "ABC"
 	//s := "ADOBECODEBANC"
 	//p := "abc"
 	//utils.QuickSort(nums, 0, len(nums)-1)
-	answer := practice.SetZeroes(matrix)
+	answer := practice.SpiralOrder(matrix)
 
 	//fmt.Printf("nums: %v\n", nums)
 	fmt.Printf("answer: %v\n", answer)
