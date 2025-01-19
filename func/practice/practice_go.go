0a635d9739bcb331d62518668ec9f5a8a6ec23c5
Dorian <tianqiyin013368@gmail.com>
2025-01-19 15:01:57 +0800

    create merge two list by go

--- func/practice/practice_go.go
+++ func/practice/practice_go.go
@@ -633,3 +633,29 @@ func DetectCycle(head *utils.ListNode) *utils.ListNode {
 
 	return slow
 }
+
+func MergeTwoLists(list1 *utils.ListNode, list2 *utils.ListNode) *utils.ListNode {
+	dummyHead := &utils.ListNode{}
+	current := dummyHead
+
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			current.Next = list1
+			list1 = list1.Next
+		} else {
+			current.Next = list2
+			list2 = list2.Next
+		}
+
+		current = current.Next
+	}
+
+	if list1 != nil {
+		current.Next = list1
+	}
+	if list2 != nil {
+		current.Next = list2
+	}
+
+	return dummyHead.Next
+}
--- main.go
+++ main.go
@@ -22,22 +22,22 @@ func main() {
 	//s := "ADOBECODEBANC"
 	//p := "abc"
 	//utils.QuickSort(nums, 0, len(nums)-1)
-	arrayA := []int{3, 2, 0, -4}
-	//arrayB := []int{5, 6, 1, 8, 4, 5}
+	arrayA := []int{1, 2, 4}
+	arrayB := []int{1, 3, 4}
 
 	headA := utils.CreateSingleLinkList(arrayA)
 
-	headA.Next.Next.Next.Next = headA.Next.Next
-	//headB := utils.CreateSingleLinkList(arrayB)
+	//headA.Next.Next.Next.Next = headA.Next.Next
+	headB := utils.CreateSingleLinkList(arrayB)
 
 	//headB.Next.Next.Next.Next = headA.Next.Next.Next
 
-	answer := practice.DetectCycle(headA.Next)
+	answer := practice.MergeTwoLists(headA.Next, headB.Next)
 
-	//for answer != nil {
-	//	fmt.Println(answer.Val)
-	//	answer = answer.Next
-	//}
+	for answer != nil {
+		fmt.Println(answer.Val)
+		answer = answer.Next
+	}
 
 	//fmt.Printf("nums: %v\n", nums)
 	fmt.Printf("answer: %v\n", answer)
