5fced4b1bf2d9d59f398d9a08133849ce8c8d415
Dorian <tianqiyin013368@gmail.com>
2025-02-08 16:03:25 +0800

    create search matrix with go

--- func/classification/search_binary.go
+++ func/classification/search_binary.go
@@ -17,5 +17,28 @@ func SearchInsert(nums []int, target int) int {
 }
 
 func SearchMatrix(matrix [][]int, target int) bool {
+	rows := len(matrix)
 
+	for i := 0; i < rows; i++ {
+		lastCol := len(matrix[i])
+		if matrix[i][lastCol-1] < target {
+			continue
+		}
+
+		left, right := 0, lastCol-1
+
+		for left <= right {
+			mid := (left + right) / 2
+
+			if matrix[i][mid] == target {
+				return true
+			} else if matrix[i][mid] < target {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+
+	return false
 }
