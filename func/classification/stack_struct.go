50a31a20d9be8f5a9a7faa009582e86fd683c009
Dorian <tianqiyin013368@gmail.com>
2025-02-10 13:05:02 +0800

    create min stack with go

--- func/classification/stack_struct.go
+++ func/classification/stack_struct.go
@@ -25,3 +25,50 @@ func IsValid(s string) bool {
 
 	return len(stack) == 0
 }
+
+type MinStack struct {
+	minStack []int
+	stack    []int
+}
+
+func Constructor() MinStack {
+	return MinStack{
+		minStack: make([]int, 0),
+		stack:    make([]int, 0),
+	}
+}
+
+func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
+	if len(this.minStack) == 0 || this.GetMin() >= val {
+		this.minStack = append(this.minStack, val)
+	}
+}
+
+func (this *MinStack) Pop() {
+	if len(this.stack) == 0 {
+		return
+	}
+	removed := this.stack[len(this.stack)-1]
+	this.stack = this.stack[:len(this.stack)-1]
+
+	if removed == this.GetMin() {
+		this.minStack = this.minStack[:len(this.minStack)-1]
+	}
+}
+
+func (this *MinStack) Top() int {
+	if len(this.stack) == 0 {
+		return -1
+	}
+
+	return this.stack[len(this.stack)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	if len(this.minStack) == 0 {
+		return -1
+	}
+
+	return this.minStack[len(this.minStack)-1]
+}
