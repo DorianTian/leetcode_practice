ca2f61a97f916834cf70741fdd2acc22db17bd22
Dorian <tianqiyin013368@gmail.com>
2025-02-15 15:20:37 +0800

    create can partition with go

--- func/classification/dynamic_programming.go
+++ func/classification/dynamic_programming.go
@@ -168,3 +168,27 @@ func MaxProduct(nums []int) int {
 
 	return result
 }
+
+func CanPartition(nums []int) bool {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+
+	if sum%2 == 1 {
+		return false
+	}
+
+	target := sum / 2
+
+	dp := make([]bool, target+1)
+	dp[0] = true
+
+	for _, num := range nums {
+		for j := target; j >= num; j-- {
+			dp[j] = dp[j] || dp[j-num]
+		}
+	}
+
+	return dp[target]
+}
