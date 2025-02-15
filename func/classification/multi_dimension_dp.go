92ef155e159f65244b499cf47466906123a782ab
Dorian <tianqiyin013368@gmail.com>
2025-02-15 22:34:52 +0800

    create min path sum with go

--- func/classification/multi_dimension_dp.go
+++ func/classification/multi_dimension_dp.go
@@ -23,3 +23,29 @@ func UniquePaths(m int, n int) int {
 
 	return dp[m-1][n-1]
 }
+
+func MinPathSum(grid [][]int) int {
+	dp := make([][]int, len(grid))
+
+	for i := range dp {
+		dp[i] = make([]int, len(grid[0]))
+	}
+
+	dp[0][0] = grid[0][0]
+
+	for i := 1; i < len(grid); i++ {
+		dp[i][0] = grid[i][0] + dp[i-1][0]
+	}
+
+	for j := 1; j < len(grid[0]); j++ {
+		dp[0][j] = grid[0][j] + dp[0][j-1]
+	}
+
+	for i := 1; i < len(grid); i++ {
+		for j := 1; j < len(grid[0]); j++ {
+			dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
+		}
+	}
+
+	return dp[len(grid)-1][len(grid[0])-1]
+}
