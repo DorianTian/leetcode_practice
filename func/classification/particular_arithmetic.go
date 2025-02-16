9e639e2f5862a6768ef70b3daf220cccb0827d9e
Dorian <tianqiyin013368@gmail.com>
2025-02-16 20:38:47 +0800

    create majority element with go

--- func/classification/particular_arithmetic.go
+++ func/classification/particular_arithmetic.go
@@ -9,3 +9,22 @@ func SingleNumber(nums []int) int {
 
 	return result
 }
+
+func MajorityElement(nums []int) int {
+	candidate := -1
+	count := 0
+
+	for _, num := range nums {
+		if count == 0 {
+			candidate = num
+		}
+
+		if num == candidate {
+			count++
+		} else {
+			count--
+		}
+	}
+
+	return candidate
+}
