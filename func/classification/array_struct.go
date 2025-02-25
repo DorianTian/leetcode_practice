f28fd7350e8594a9b11d3bc0b0357273f73b5fdc
Dorian <tianqiyin013368@gmail.com>
2025-02-25 21:34:41 +0800

    add max profit2 with go
    Signed-off-by: Dorian <tianqiyin013368@gmail.com>

--- func/classification/array_struct.go
+++ func/classification/array_struct.go
@@ -190,3 +190,14 @@ func MaxProfit(prices []int) int {
 
 	return maxProfit
 }
+
+func MaxProfit2(prices []int) int {
+	maxProfit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			maxProfit += prices[i] - prices[i-1]
+		}
+	}
+
+	return maxProfit
+}
