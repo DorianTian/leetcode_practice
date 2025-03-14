9a32e6c96cb53362a6cc5ccd8d81691a2327e8a0
Dorian <tianqiyin013368@gmail.com>
2025-03-14 12:58:32 +0800

    create isSubsequence with go

--- func/classification/array_struct.go
+++ func/classification/array_struct.go
@@ -489,3 +489,18 @@ func IsPalindrome(s string) bool {
 
 	return true
 }
+
+func IsSubsequence(s string, t string) bool {
+	i := 0
+	j := 0
+
+	for i < len(s) && j < len(t) {
+		if s[i] == t[j] {
+			i++
+		}
+
+		j++
+	}
+
+	return i == len(s)
+}
