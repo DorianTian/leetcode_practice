39c288873684e2caa4f2283f0e628a603cc7c977
Dorian <tianqiyin013368@gmail.com>
2025-03-25 22:29:24 +0800

    create isAnagram with go

--- func/classification/array_struct.go
+++ func/classification/array_struct.go
@@ -894,3 +894,25 @@ func WordPattern(pattern string, s string) bool {
 
 	return true
 }
+
+func IsAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	count := make(map[rune]int)
+
+	for _, char := range s {
+		count[char]++
+	}
+
+	for _, char := range t {
+		count[char]--
+
+		if count[char] < 0 {
+			return false
+		}
+	}
+
+	return true
+}
