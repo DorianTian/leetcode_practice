13668043f10f0be055207e4521709c87e036ab11
Dorian <tianqiyin013368@gmail.com>
2025-03-30 17:12:25 +0800

    add summaryRanges function to generate ranges from sorted integer slices in Go

--- func/classification/array_struct.go
+++ func/classification/array_struct.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"sort"
+	"strconv"
 	"strings"
 	"unicode"
 )
@@ -1037,4 +1038,29 @@ func longestConsecutive(nums []int) int {
 	}
 
 	return longestStreak
+}
+
+func summaryRanges(nums []int) []string {
+	var result []string
+	if len(nums) == 0 {
+		return result
+	}
+
+	start := nums[0]
+
+	for i := 1; i <= len(nums); i++ {
+		if i == len(nums)-1 || nums[i] != nums[i-1]+1 {
+			if start == nums[i-1] {
+				result = append(result, strconv.Itoa(start))
+			} else {
+				result = append(result, strconv.Itoa(start)+"->"+strconv.Itoa(nums[i-1]))
+			}
+
+			if i < len(nums) {
+				start = nums[i]
+			}
+		}
+	}
+
+	return result
 }
