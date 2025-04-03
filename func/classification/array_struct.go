37625eeb6620d53c36f7d41e9b3859c9575268eb
Dorian <tianqiyin013368@gmail.com>
2025-04-03 20:00:45 +0800

    add EvalRPN function to evaluate Reverse Polish Notation expressions in Go

--- func/classification/array_struct.go
+++ func/classification/array_struct.go
@@ -1173,3 +1173,38 @@ func simplifyPath(path string) string {
 
 	return "/" + strings.Join(stack, "/")
 }
+
+// tokens = ["2","1","+","3","*"]
+func EvalRPN(tokens []string) int {
+	stack := []int{}
+
+	for _, token := range tokens {
+		switch token {
+		case "+", "-", "*", "/":
+			b := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			a := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			var result int
+			switch token {
+			case "+":
+				result = a + b
+			case "-":
+				result = a - b
+			case "*":
+				result = a * b
+			case "/":
+				result = a / b
+			}
+
+			stack = append(stack, result)
+
+		default:
+			num, _ := strconv.Atoi(token)
+			stack = append(stack, num)
+		}
+	}
+
+	return stack[0]
+}
