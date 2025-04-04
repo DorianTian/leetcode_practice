d731de7f18707b9328943f2b0a05018c460d3b0c
Dorian <tianqiyin013368@gmail.com>
2025-04-04 16:38:16 +0800

    add addTwoNumbers function to sum two linked lists in Go

--- func/classification/link_list_struct.go
+++ func/classification/link_list_struct.go
@@ -335,3 +335,29 @@ func hasCycle(head *utils.ListNode) bool {
 
 	return false
 }
+
+func addTwoNumbers(l1 *utils.ListNode, l2 *utils.ListNode) *utils.ListNode {
+	dummyHead := &utils.ListNode{}
+	current := dummyHead
+	carry := 0
+
+	for l1 != nil || l2 != nil || carry != 0 {
+		sum := carry
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+
+		carry = sum / 10
+
+		current.Next = &utils.ListNode{Val: sum % 10}
+		current = current.Next
+	}
+
+	return dummyHead.Next
+}
