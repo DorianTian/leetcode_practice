7506773a6f545f339931da14f82a40db4868a507
Dorian <tianqiyin013368@gmail.com>
2025-04-05 22:16:56 +0800

    add mergeTwoLists function to merge two sorted linked lists in Go

--- func/classification/link_list_struct.go
+++ func/classification/link_list_struct.go
@@ -361,3 +361,30 @@ func addTwoNumbers(l1 *utils.ListNode, l2 *utils.ListNode) *utils.ListNode {
 
 	return dummyHead.Next
 }
+
+func mergeTwoLists(list1 *utils.ListNode, list2 *utils.ListNode) *utils.ListNode {
+	dummyHead := &utils.ListNode{}
+	current := dummyHead
+
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			current.Next = list1
+			list1 = list1.Next
+		} else {
+			current.Next = list2
+			list2 = list2.Next
+		}
+
+		current = current.Next
+	}
+
+	if list1 != nil {
+		current.Next = list1
+	}
+
+	if list2 != nil {
+		current.Next = list2
+	}
+
+	return dummyHead.Next
+}
